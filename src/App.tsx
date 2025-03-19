613b30e419286b7e6cb89030098eecbdf6cc47f6
Kai <Pappa@DESKTOP-29NAR0C>
2025-03-19 13:30:54 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-first text-fourth text-[34px] h-screen flex items-center justify-center">
+        <h1>ToDO</h1>
+
+       </div>
+    </>
+  )
+}
+
+export default App
